fef98f769816a2c9b28d38e13d6e56594e89bdc8
Divine Dube <216004854@student.uj.ac.za>
2021-08-31 23:54:01 +0400

    add pg support

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@
 module.exports = ({ env }) => ({
   "github-publish": {
     owner: "dubeboy", // The gothub organisation or user
-    repo: "next-app", // The name of the repository
+    repo: "dubedivine_blog", // The name of the repository
     workflow_id: "main.yml", // The workflow_id or filename
     token: env("GITHUB_TOKEN"), // The GitHub personal access token with access to trigger workflows and view build status
     branch: "main", // The branch the workflow should be triggered on
